a2acdb3b57efb6a0f4e52263f5429d2cf809d78b
hatoXD <takka.taka0326@gmail.com>
2024-01-20 00:56:09 +0900

    Updates

---
+++ precache-manifest.b8a191a79a076920e9b677cf2fd1a2e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73747acab1bf1baae929b57cdf2b0ab6",
+    "url": "/sana/index.html"
+  },
+  {
+    "revision": "10c7fc7368ccc856d52d",
+    "url": "/sana/static/css/main.85249075.chunk.css"
+  },
+  {
+    "revision": "f86f7a9b94aa23be7f29",
+    "url": "/sana/static/js/2.91dc03cb.chunk.js"
+  },
+  {
+    "revision": "10c7fc7368ccc856d52d",
+    "url": "/sana/static/js/main.645a1d80.chunk.js"
+  },
+  {
+    "revision": "c05c7273dd3d5f910b75",
+    "url": "/sana/static/js/runtime~main.8756fb09.js"
+  }
+]);
